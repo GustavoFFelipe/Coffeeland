18b97fa8b0e524892eb530324c43c26d09648628
Gustavo Felipe <gustavo.ffelipedev@gmail.com>
2022-11-17 21:11:13 -0300

    Header finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 
+import blurUp from './assets/blur-2.png';
+import blurDown from './assets/blur-1.png';
+
+import './global.scss';
 import './App.scss'
+import { Header } from './components/Header';
 
 function App() {
 
   return (
-    <>
+    <div className="container">
+      {/* ------------efetito Blur---------------*/}
+      <div className="blurContainer">
+              <div id="blurUp">
+                  <img  src={blurUp} alt="" />
+              </div>
+              <div id='blurDown'>
+                  <img  src={blurDown} alt="" />
+              </div>
+      </div>
 
-    </>
+      <Header />
+
+
+
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
+import './Header.scss';
 
+import coffee  from '../assets/coffee-icon.png';
 
 export function Header(){
     return(
         <div className="headerContainer">
-            
+            <a href="#">
+                <img src={coffee} alt="" />
+            </a>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Cafés</a>
+                <a href="">Recompensas</a>
+                <a href="">Gift Card</a>
+                <a href="">Lojas</a>
+            </nav>
+            <button className="askCoffee">
+                Pegar meu café
+            </button>
         </div>
     )
 }
